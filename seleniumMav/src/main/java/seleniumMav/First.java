51ce958a4e0e936d328c2d8b70f9aa202abb35f2
Jamnesh Maniya <101506080+Jamneshmaniya15@users.noreply.github.com>
2026-02-26 16:01:38 +0530

    Update First.java

--- seleniumMav/src/main/java/seleniumMav/First.java
+++ seleniumMav/src/main/java/seleniumMav/First.java
@@ -2,23 +2,23 @@ package seleniumMav;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class First {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        
+        // Setup ChromeOptions for Headless Mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Required for Jenkins automation
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com/");
-		
-		System.out.println("Title : " +driver.getTitle());
-		
-		driver.quit();
-
-	}
-
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+        System.out.println("Title : " + driver.getTitle());
+        driver.quit();
+    }
 }
